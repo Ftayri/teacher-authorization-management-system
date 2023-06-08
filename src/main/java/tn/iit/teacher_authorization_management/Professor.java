fcb5af5bcdf157833125848557244f78429f6a29
Iyed <75183184+Ftayri@users.noreply.github.com>
2023-06-08 14:47:35 +0100

    Implement Hibernate configuration and entity mappings

---
+++ src/main/java/tn/iit/teacher_authorization_management/Professor.java
@@ -0,0 +1,29 @@
+package tn.iit.teacher_authorization_management;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "professors")
+public class Professor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	private String cin;
+
+	private String email;
+
+	// Constructors, getters, and setters
+}
