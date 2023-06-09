3853a759b7f85c82e6a3de9c91d1bb6a0840855a
Iyed <75183184+Ftayri@users.noreply.github.com>
2023-06-09 10:46:11 +0100

    Add constructors, getters and setters to entities

--- src/main/java/tn/iit/teacher_authorization_management/Professor.java
+++ src/main/java/tn/iit/teacher_authorization_management/Professor.java
@@ -25,5 +25,52 @@ public class Professor {
 
 	private String email;
 
-	// Constructors, getters, and setters
+	public Professor(Long id, String firstName, String lastName, String cin, String email) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.cin = cin;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCin() {
+		return cin;
+	}
+
+	public void setCin(String cin) {
+		this.cin = cin;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
