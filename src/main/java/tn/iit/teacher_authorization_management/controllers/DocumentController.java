ea799612c61768ecff6faac62764777b93489620
Iyed <75183184+Ftayri@users.noreply.github.com>
2023-06-10 16:03:15 +0100

    Finished document generation

--- src/main/java/tn/iit/teacher_authorization_management/controllers/DocumentController.java
+++ src/main/java/tn/iit/teacher_authorization_management/controllers/DocumentController.java
@@ -3,15 +3,19 @@ package tn.iit.teacher_authorization_management.controllers;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import tn.iit.teacher_authorization_management.Admin;
 import tn.iit.teacher_authorization_management.Professor;
 import tn.iit.teacher_authorization_management.dao.ProfessorDAO;
 import tn.iit.teacher_authorization_management.util.HibernateUtil;
+import tn.iit.teacher_authorization_management.util.PDFGenerator;
 
 /**
  * Servlet implementation class DocumentController
@@ -48,6 +52,17 @@ public class DocumentController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Long id = Long.parseLong(request.getParameter("id"));
 		Professor professor = professorDAO.getProfessorById(id);
+		HttpSession httpSession = request.getSession();
+		Admin admin = (Admin) httpSession.getAttribute("admin");
+		if(admin==null) {
+			admin = new Admin();
+			admin.setUsername("test");
+		}
+		PDFGenerator.generatePDF(professor, admin);
+		request.setAttribute("pdfSuccess", "Printable generated successfully");
+		professor.setAuthorized(true);
+		professorDAO.updateProfessor(professor);
+		doGet(request,response);
 	}
 
 }
---
+++ src/main/java/tn/iit/teacher_authorization_management/util/PDFGenerator.java
@@ -0,0 +1,79 @@
+package tn.iit.teacher_authorization_management.util;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import javax.servlet.ServletContext;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
+import tn.iit.teacher_authorization_management.Admin;
+import tn.iit.teacher_authorization_management.Professor;
+
+public class PDFGenerator {
+	public static void generatePDF(Professor professor, Admin admin) {
+		int weeksLeft = 52 - Calendar.getInstance().get(Calendar.WEEK_OF_YEAR);
+		try {
+			PDDocument document = new PDDocument();
+			PDPage page = new PDPage(PDRectangle.A4);
+			document.addPage(page);
+
+			PDPageContentStream contentStream = new PDPageContentStream(document, page);
+			PDImageXObject logoImage = PDImageXObject.createFromFile("F:\\Cours\\2eme\\JEE\\Teacher Authorization Management System\\src\\main\\resources\\logo.png", document);
+
+			contentStream.drawImage(logoImage, 25, 775, 50, 50);
+
+			contentStream.setFont(PDType1Font.TIMES_BOLD, 25);
+			contentStream.beginText();
+			contentStream.newLineAtOffset(200,800);
+			contentStream.showText("Work Authorization");
+			contentStream.endText();
+
+			contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
+			contentStream.setLeading(15f);
+			contentStream.beginText();
+			contentStream.newLineAtOffset(50, 650);
+
+			String introText = "I, " + admin.getUsername() + ", the undersigned hereby acknowledge that Professor "
+			        + professor.getFirstName() + " " + professor.getLastName() + " of ID " + professor.getCin()
+			        + " is allowed " + weeksLeft * 4 + " working hours  for expertise activities and/or"
+			        		+ " training in continuing vocational education and training.";
+			float maxWidth = 500;
+
+			String[] words = introText.split("\\s+");
+			StringBuilder line = new StringBuilder();
+			for (String word : words) {
+			    float width = PDType1Font.TIMES_ROMAN.getStringWidth(line.toString() + " " + word) / 1000 * 12;
+			    System.out.println(width);
+			    if (width > maxWidth) {
+			        contentStream.showText(line.toString().trim()); 
+			        contentStream.newLine();
+			        line = new StringBuilder(word + " ");
+			    } else {
+			        line.append(word).append(" ");
+			    }
+			}
+			contentStream.showText(line.toString().trim());
+			contentStream.setLeading(25f);
+			contentStream.newLine();
+			contentStream.setFont(PDType1Font.TIMES_BOLD, 15);
+			contentStream.showText("Signature: ");
+			contentStream.newLine();
+			contentStream.showText("_______________");
+			contentStream.endText();
+			contentStream.close();
+
+			document.save("F:\\Cours\\2eme\\JEE\\Teacher Authorization Management System\\src\\main\\resources\\authorization"+professor.getFirstName()+"_"+professor.getLastName()+".pdf");
+			document.close();
+
+			System.out.println("PDF generated successfully.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
